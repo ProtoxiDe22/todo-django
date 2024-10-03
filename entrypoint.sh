d1e75d897b29f635e0ef97fd5744d43b16070749
Fabio De Simone <prototypefab@gmail.com>
2024-10-03 11:06:56 +0200

    Updated Project files

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Apply database migrations
+echo "Applying database migrations..."
+python manage.py migrate
+
+# Start server
+echo "Starting server..."
+gunicorn --bind 0.0.0.0:8000 todo_project.wsgi:application
