2948359ddadd3fc8de63c874bb0c03798afcdb99
Fabio De Simone <prototypefab@gmail.com>
2024-10-03 17:52:10 +0200

    use urlpattern for users api endpoint

--- todo/API/urls.py
+++ todo/API/urls.py
@@ -4,11 +4,11 @@ from rest_framework.routers import DefaultRouter
 from .api_views import UserViewSet, PersonalTaskViewSet, AllTaskViewSet
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='User')
 router.register(r'tasks', PersonalTaskViewSet, basename='Task')
 
 urlpatterns = [
     path('tasks/all/', AllTaskViewSet.as_view({'get': 'list'}), name='all-tasks'),
+    path('users/', UserViewSet.as_view({'get': 'list'}), name='users'),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
